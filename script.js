c2305bd4fff0d12f8edef05d5268e98c7dddc9f6
Sam Chung <chungyiusingsam@gmail.com>
2024-03-22 20:28:10 +0800

    add comments for the html and script

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
+// declare the variable for the element by id
 const calorieCounter = document.getElementById('calorie-counter');
 const budgetNumberInput = document.getElementById('budget');
 const entryDropdown = document.getElementById('entry-dropdown');
 const addEntryButton = document.getElementById('add-entry');
 const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
+
+// variable for the error flag
 let isError = false;
 
+// function for clearing the specific symbol or character 
 function cleanInputString(str) {
-    const regex = /[+-\s]/g;
-    return str.replace(regex, '');
+    const regex = /[+-\s]/g; // set the regular expression 
+    return str.replace(regex, ''); // use the replace the function to replace it
 }
 
+// function for checking a valid character
 function isInvalidInput(str) {
     const regex = /\d+e\d+/i;
     return str.match(regex);
 }
 
 function addEntry() {
+    // get the container value from the element and use the `` to pass the value to string
+    // the input-container is the class and the entryDropdown.value is class also
     const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
     const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
+
+    // create a new expression for the HTML string
     const HTMLString = `
   <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
   <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
@@ -32,16 +41,19 @@ function addEntry() {
     targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
 }
 
+
 function calculateCalories(e) {
     e.preventDefault();
     isError = false;
 
+    // target the field by using class
     const breakfastNumberInputs = document.querySelectorAll('#breakfast input[type=number]');
     const lunchNumberInputs = document.querySelectorAll('#lunch input[type=number]');
     const dinnerNumberInputs = document.querySelectorAll('#dinner input[type=number]');
     const snacksNumberInputs = document.querySelectorAll('#snacks input[type=number]');
     const exerciseNumberInputs = document.querySelectorAll('#exercise input[type=number]');
 
+    // get the inputs from the input field 
     const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
     const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
     const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
@@ -49,13 +61,16 @@ function calculateCalories(e) {
     const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
     const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
 
+    // if have invalid input will return
     if (isError) {
         return;
     }
 
+    // new variables for calculation
     const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
     const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
     const surplusOrDeficit = remainingCalories < 0 ? 'Surplus' : 'Deficit';
+    // the abs() is for absolute value
     output.innerHTML = `
   <span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
   <hr>
@@ -67,6 +82,7 @@ function calculateCalories(e) {
     output.classList.remove('hide');
 }
 
+// the function is getting input field values 
 function getCaloriesFromInputs(list) {
     let calories = 0;
 
@@ -74,6 +90,7 @@ function getCaloriesFromInputs(list) {
         const currVal = cleanInputString(item.value);
         const invalidInputMatch = isInvalidInput(currVal);
 
+        // invalid input will return null value and return alert message!!!
         if (invalidInputMatch) {
             alert(`Invalid Input: ${invalidInputMatch[0]}`);
             isError = true;
@@ -84,7 +101,9 @@ function getCaloriesFromInputs(list) {
     return calories;
 }
 
+// the function is for clear all innerHTMLs
 function clearForm() {
+    // get all containers which the name is input-container
     const inputContainers = Array.from(document.querySelectorAll('.input-container'));
 
     for (const container of inputContainers) {
@@ -96,6 +115,7 @@ function clearForm() {
     output.classList.add('hide');
 }
 
+// for the event listener and function call
 addEntryButton.addEventListener("click", addEntry);
 calorieCounter.addEventListener("submit", calculateCalories);
 clearButton.addEventListener("click", clearForm);
